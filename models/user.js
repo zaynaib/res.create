a523ccb715385fac126ee6b09db3d42656cfa88c
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2017-11-03 11:40:41 -0500

    1

--- models/user.js
+++ models/user.js
@@ -23,7 +23,7 @@ module.exports = function(sequelize, DataTypes){
         Website: {
             type: DataTypes.STRING
         }, 
-    } ({timestamps: false}) )
+    }, ({timestamps: false}) )
 
     return User;
 };

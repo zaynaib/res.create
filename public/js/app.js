fa53238a0d90695e3eff30e9fa1a33284e2c467c
Scott Foster <thescottfoster@gmail.com>
2017-11-03 14:33:27 -0500

    added form data retrieval

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,11 @@ var testData = [{name: "full-name", value: "Mr Hats"},
 {name: "state", value: "IL"},
 {name: "name", value: "60647"}];
 
+var userID = 1;
+
 // -- -- Event Handlers -- --
 
+// Materialize Datepicker
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 20, // Creates a dropdown of 15 years to control year,
@@ -15,7 +18,8 @@ $('.datepicker').pickadate({
 	closeOnSelect: false // Close upon selecting a date,
 });
 
-$('.save-btn').click( function(){
+//When Save Button clicked, User form data added to db
+$('.save-user-btn').click( function(){
 
 	var newData = $('#info').serializeArray();
 
@@ -29,8 +33,79 @@ $('.save-btn').click( function(){
       alert("Adding data...");
     });
 
-	console.log($('#info').serializeArray());
+	//console.log($('#info').serializeArray());
 
+});
+
+//If user exists in database, populates the info fields
+
+$.get("/api/user/"+userID)
+
+	.done(function(data){
+
+		console.log(data);
+
+		$('#body-container').html(`
+			<div id="card1" class="card">
+			
+				<div class="card-content">
+					<span class="card-title">Personal Info</span>
+
+						<div class="row">
+							<form id="info" class="col s12">
+
+								<div class="row">
+									<div class="input-field col s12">
+										<input value="${data.Name}" id="info-fullname" name="Name" type="text" class="validate">
+										<label for="info-fullname">Full Name</label>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="input-field col s12">
+										<input value="${data.Address}" id="info-street-address" name="Address" type="text" class="validate">
+										<label for="info-street-address">Street Address</label>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="input-field col s6">
+										<input value="${data.City}" id="info-city" name="City" type="text" class="validate">
+										<label for="info-city">City</label>
+									</div>
+									<div class="input-field col s3">
+										<input value="${data.State}" id="info-state" name="State" type="text" class="validate">
+										<label for="info-state">State</label>
+									</div>
+									<div class="input-field col s3">
+										<input value="${data.Zipcode}" id="info-zipcode" name="Zipcode" type="text" class="validate">
+										<label for="info-zipcode">ZIP Code</label>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="input-field col s6">
+										<input value="${data.Email}" id="info-email" name="Email" type="text" class="validate">
+										<label for="info-email">Email Address</label>
+									</div>
+									<div class="input-field col s6">
+										<input value="${data.Phone}" id="info-phone-number" name="Phone" type="text" class="validate">
+										<label for="info-phone-number">Phone Number</label>
+									</div>
+								</div>
+				
+								<div class="row">
+									<a class="waves-effect waves-light right save-user-btn btn"><i class="material-icons right">done</i>Save</a>
+								</div>
+
+							</form>
+						</div>
+					
+				</div>
+			</div>
+
+		</div>
+	`);
 });
 
 
--- routing/api-routes.js
+++ routing/api-routes.js
@@ -2,19 +2,19 @@ var db = require('../models');
 
 module.exports = function(app) {
 // Find All Users and return results in json format
-    app.get('api/user', function(req,res) {
+    app.get('/api/user', function(req,res) {
         db.User.findAll({}).then(function(dbUser){
             res.json(dbUser);
         });
     });
 
-    app.get('api/user/:id', function(req,res){
+    app.get('/api/user/:id', function(req,res){
     // Find Users and return them in Json format 
         db.User.findOne({
             where: {
                 id: req.params.id
             }
-        }).then(function(dbuser){
+        }).then(function(dbUser){
             res.json(dbUser);
         });
     });
@@ -27,7 +27,7 @@ module.exports = function(app) {
         });
     });
 
-    app.delete('api/User/:id', function(req,res){
+    app.delete('/api/User/:id', function(req,res){
     // Deletes User with and specified ID
         db.User.destroy({
             where: {
@@ -37,7 +37,7 @@ module.exports = function(app) {
             res.json(dbUser);
         });
     });
-    
+
 
     // // Updated User Info 
     // app.put('/api/user', function(req,res){

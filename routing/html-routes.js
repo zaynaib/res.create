47670d8236f1e2410759bc162ddbc6d4a201feb8
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2017-11-02 20:30:55 -0500

    new files

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -3,11 +3,22 @@ var path = require('path');
 module.exports = function(app){
 
 
-    app.get('/signup', function(req,res){
-        res.sendFile(path.join(__dirname, '/../public/signup.html'))
+    app.get('/bio', function(req,res){
+        res.sendFile(path.join(__dirname, '/../public/bio.html'))
     })
 
+    app.get('/education', function(req,res){
+        res.sendFile(path.join(__dirname, '/../public/edu.html'))
+    })
     
+    app.get('/skills', function(req,res){
+        res.sendFile(path.join(__dirname, '/../public/bio.html'))
+    })
+    
+    app.get('/references', function(req,res){
+        res.sendFile(path.join(__dirname, '/../public/ref.html'))
+    })
+
     app.use(function(req,res){
         res.sendFile(path.join(__dirname, '/../public/index.html'))
     })

d15178b7ac4370839602febdefed994acea9ce63
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2017-11-07 10:07:37 -0600

    routes

--- routing/user-api-routes.js
+++ routing/user-api-routes.js
@@ -1,6 +1,4 @@
 var db = require('../models');
-var bcrypt = require('bcrypt');
-var salt = bcrypt.genSaltSync(10);
 
 
 module.exports = function(app) {
@@ -59,30 +57,6 @@ module.exports = function(app) {
     });
     
 });
-
-
-// ==================================User Auth Post===============================================
-app.post('/signin', function(req,res){
-    db.User.findOne({
-        username: req.body.username
-    }).then(function(user){
-        if (!user){
-            res.status(400).json({
-                'status' : 'Invalid Username & Password'
-            })
-        } else {
-            bcrypt.compare(req.body.password, user.password, function(err,valid){
-                res.status(400).json({
-                    'status' : 'Invalid Username & Password'
-                })
-            })
-        }
-        res.status(200).json({
-            id: user.id,
-            username: user.username
-        });
-    })
-})
 
 
 }//end of export
